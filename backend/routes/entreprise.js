c9b3757ac9251eed5090e3389369f94492208a27
MOHAMED MAHAMOUD NAGUIB <naguib.mohamed-mahamoud@etu.univ-lyon1.fr>
2023-03-06 18:35:45 +0100

    Ajout des routes PUT, DELETE et GET pour l'entité entreprise

--- backend/routes/entreprise.js
+++ backend/routes/entreprise.js
@@ -2,17 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Entreprise = require('../models/entreprise');
 
-// Get all entreprises
-
+// GET toutes les entreprises
 router.get('/entreprises', async (req, res) => {
     try {
-        const entreprises = await Entreprise.find({});
+        const entreprises = await Entreprise.find();
         res.json(entreprises);
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        console.error(err.message);
+        res.status(500).send('Server Error');
     }
 });
 
+// POST une nouvelle entreprise
 router.post('/entreprises', async (req, res) => {
     try {
         const nouvelleEntreprise = new Entreprise(req.body);
@@ -20,6 +21,38 @@ router.post('/entreprises', async (req, res) => {
         res.status(201).json(entreprise);
     } catch (err) {
         res.status(500).json({ message: err.message });
+    }
+});
+
+// GET une entreprise par ID
+router.get('/entreprises/:id', async (req, res) => {
+    try {
+        const entreprise = await Entreprise.findById(req.params.id);
+        res.json(entreprise);
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
+});
+
+// PUT mettre à jour une entreprise
+router.put('/entreprises/:id', async (req, res) => {
+    try {
+        const entreprise = await Entreprise.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(entreprise);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// DELETE supprimer une entreprise
+router.delete('/entreprises/:id', async (req, res) => {
+    try {
+        const entreprise = await Entreprise.findByIdAndDelete(req.params.id);
+        res.json(entreprise);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
     }
 });
 
